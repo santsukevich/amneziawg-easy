172d33e12242720437c24e4e96c368513d9dde84
Sergey Antsukevich <antsukevich@gmail.com>
2025-06-25 15:42:58 +0300

    No need to manually load env vars in run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 # Set variables from .env file
-export $(grep -v '^#' .env | xargs -d '\n')
+# export $(grep -v '^#' .env | xargs -d '\n')
 
 # Start containers
 docker compose -f ./docker-compose.traefik.yml up -d
